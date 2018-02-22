065f1028e84ff0eb51b1c76b5e06397e974ef5c2
asingh4 <singh.abhi202@gmail.com>
2018-02-23 02:41:57 +0530

    LoginPopUp Model Implemented

---
+++ src/app/login-header/login-header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-header',
+  templateUrl: './login-header.component.html',
+  styleUrls: ['./login-header.component.css']
+})
+export class LoginHeaderComponent implements OnInit {
+
+  companyName : String = 'SimplexMart';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
